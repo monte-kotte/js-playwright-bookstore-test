f137e87a3542d9b83fbc4220ab3418ef6d7b3e7a
Ekaterina Karpuk <kate.ff.usa@gmail.com>
2024-11-22 16:24:15 +0100

    Update playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -1,34 +1,16 @@
 // @ts-check
 const { defineConfig } = require('@playwright/test');
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
-/**
- * @see https://playwright.dev/docs/test-configuration
- */
 module.exports = defineConfig({
   testDir: './tests',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 1 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  workers: process.env.CI ? 1 : 1,
+  reporter: process.env.CI ? 'list' : 'html',
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    baseURL: 'https://bookcart.azurewebsites.net',
     trace: 'on-first-retry',
   },
 
@@ -40,10 +22,8 @@ module.exports = defineConfig({
         headless: true,
         screenshot: 'only-on-failure',
         video: 'retain-on-failure',
-        baseURL: 'https://bookcart.azurewebsites.net',
         trace: 'on-first-retry'
       },
     },
   ],
 });
-
