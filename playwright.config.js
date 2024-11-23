9493e77c373d2f9e672f037076ddaac8513b05be
Ekaterina Karpuk <kate.ff.usa@gmail.com>
2024-11-23 15:08:13 +0100

    Update playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -6,9 +6,13 @@ require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
 module.exports = defineConfig({
   testDir: './tests',
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 10000,
+  },
   retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? 1 : 1,
-  reporter: process.env.CI ? 'list' : 'html',
+  reporter: 'list',
   use: {
     baseURL: 'https://bookcart.azurewebsites.net',
     trace: 'on-first-retry',
@@ -21,7 +25,6 @@ module.exports = defineConfig({
         browserName: 'chromium',
         headless: true,
         screenshot: 'only-on-failure',
-        video: 'retain-on-failure',
         trace: 'on-first-retry'
       },
     },
